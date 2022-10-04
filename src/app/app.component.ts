4d130eb2fd037eccd76318284b7bd100a450d149
Arnold Charris <acharris@consware.com.co>
2022-10-04 17:49:24 -0500

    Envio con validacion por producto.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,64 @@
 import { Component } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { faker } from '@faker-js/faker';
+import { FormControl } from '@angular/forms';
+
+export interface Producto {
+  codigo: string;
+  nombre: string;
+  precio: string;
+  descripcion: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'Prototipos';
+
+  public producto: Producto = {
+    codigo: 'COD001',
+    nombre: 'Muebles',
+    precio: '2100000',
+    descripcion: 'Estas son las caracteristicas del mueble'
+  };
+
+  codigoDispositivo = new FormControl('');
+
+  constructor(private _socket: Socket) { }
+
+  ngOnInit(): void {
+    this.getMessage();
+  }
+
+  cambiarInfo() {
+    const producto: Producto = {
+      codigo: '',
+      nombre: faker.commerce.productName(),
+      precio: faker.commerce.price(0, 1000000, 2, '$'),
+      descripcion: faker.lorem.sentences(),
+    };
+    this.sendMessage(producto);
+  }
+
+  sendMessage(msg: any) {
+    this._socket.emit('message', msg);
+  }
+
+  getMessage() {
+    this._socket.on('message', (response: any) => {
+      const codigo = this.codigoDispositivo.value;
+      if (response.codigo === codigo) {
+        this.producto = {
+          codigo: response.codigo,
+          nombre: response.nombre,
+          precio: response.precio,
+          descripcion: response.descripcion
+        };
+      }
+    });
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { SocketIoConfig, SocketIoModule } from 'ngx-socket-io';
+import { ReactiveFormsModule } from '@angular/forms';
+
+const config: SocketIoConfig = { url: 'http://localhost:3000', options: {} };
 
 @NgModule({
   declarations: [
@@ -10,7 +18,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+
+    MatCardModule,
+    MatButtonModule,
+    MatInputModule,
+
+    SocketIoModule.forRoot(config)
   ],
   providers: [],
   bootstrap: [AppComponent]
